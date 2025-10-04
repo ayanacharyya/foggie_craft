477c43ae1fb80f8a8b1bfca7b3a5fdbffab2a0a0
ayanacharyya <acharyyaayan@gmail.com>
2025-10-04 03:22:52 +0200

    deleted

--- utils/get_proper_box_size.py
+++
@@ -1,4 +0,0 @@
-import yt
-
-def get_proper_box_size(ds):
-    return ds.get_parameter('CosmologyComovingBoxSize') / ((1 + ds.current_redshift) * ds.get_parameter('CosmologyHubbleConstantNow')) * 1000. # in kpc
