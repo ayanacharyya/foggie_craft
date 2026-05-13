35b349ed58928f1a4b2b099d6ecd901f0ef3724e
ayanacharyya <acharyyaayan@gmail.com>
2026-05-13 19:45:29 +0200

    minor debug

--- jobscript_ldan_gmsfr.sh
+++ jobscript_ldan_gmsfr.sh
@@ -34,6 +34,6 @@ export PYTHONPATH=$PYTHONPATH:/home5/aachary2/miniconda3/envs/py380/bin/
 cd /nobackupp19/aachary2/foggie_craft/pleiades_workdir
 
 ##### Execute Program #####
-top -b -u $USER -n 259200 1>top_ldan_ghdm_all.out &
+top -b -u $USER -n 259200 1>top_ldan_gmsfr.out &
 mpiexec -n 16 python /nobackupp19/aachary2/foggie_craft/foggie_craft/get_mass_sfr.py --system ayan_pleiades 1>output_ldan_gmsfr.out 2>&1
 killall 'top'
