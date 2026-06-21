c97539a33720202faaa18a3f184882bf1cf8c9b4
ayanacharyya <acharyyaayan@gmail.com>
2026-06-21 16:37:38 +0200

    testing

--- get_mass_sfr.py
+++ get_mass_sfr.py
@@ -205,7 +205,9 @@ if __name__ == '__main__':
 
             # -------------loop over snapshots-----------------
             #print_mpi('Operating on snapshots ' + str(core_start + 1) + ' to ' + str(core_end + 1) + ', i.e., ' + str(core_end - core_start + 1) + ' out of ' + str(total_snaps) + ' snapshots', args)
+            print_mpi(f'halo {thishalo}: outputs_todo = {outputs_todo}') ##
             print_mpi(f'Operating on snapshots {thishalo:} {outputs_todo[core_start : core_end + 1]} ' + str(core_start + 1) + ' to ' + str(core_end + 1) + ', i.e., ' + str(core_end - core_start + 1) + ' out of ' + str(total_snaps) + ' snapshots', args) ##
+            
             for index in range(core_start + args.start_index, core_end + 1):
                 start_time_this_snapshot = datetime.now()
                 args.output = outputs_todo[index]
